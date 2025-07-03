13ba9d5dd6e62ba936a7521f989514bc4820eaea
Anahit <anahit.leuven@gmail.com>
2025-07-03 18:49:32 +0300

    Local docker

--- src/features/api/accountApi.ts
+++ src/features/api/accountApi.ts
@@ -1,87 +1,90 @@
-import {createAsyncThunk} from "@reduxjs/toolkit";
-import {UserData, UserProfile, UserRegister} from "../../utils/types";
-import {base_url, createToken} from "../../utils/constants.ts";
-import {RootState} from "../../app/store.ts";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { UserData, UserProfile, UserRegister } from "../../utils/types";
+import { base_url, createToken } from "../../utils/constants.ts";
+import { RootState } from "../../app/store.ts";
 
 export const registerUser = createAsyncThunk(
     'users/register',
     async (user: UserRegister) => {
-        const response = await fetch(`${base_url}/user`, {
-            method: 'Post',
+        const response = await fetch(`${base_url}/register`, {
+            method: 'POST',
             body: JSON.stringify(user),
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
             }
-        })
+        });
         if (response.status === 409) {
-            throw new Error(`user ${user.login} already exists`)
+            throw new Error(`user ${user.login} already exists`);
         }
         if (!response.ok) {
             throw new Error('Something went wrong');
         }
         const data = await response.json();
         const token = createToken(user.login, user.password);
-        return {user: data, token};
+        return { user: data, token };
     }
-)
+);
 
 export const fetchUser = createAsyncThunk(
     'users/fetch',
     async (token: string) => {
         const response = await fetch(`${base_url}/login`, {
-            method: 'Post',
+            method: 'POST',
             headers: {
-                Authorization: token
+                Authorization: token,
             }
-        })
+        });
         if (response.status === 401) {
-            throw new Error(`login or password incorrect`)
+            throw new Error(`login or password incorrect`);
         }
         if (!response.ok) {
             throw new Error('Something went wrong');
         }
         const data = await response.json();
-        return {user: data, token};
+        return { user: data, token };
     }
-)
+);
 
-export const updateUser = createAsyncThunk<UserProfile, UserData, {state: RootState}>(
+export const updateUser = createAsyncThunk<UserProfile, UserData, { state: RootState }>(
     'users/update',
-    async (user, {getState}) => {
-        const response = await fetch(`${base_url}/user`, {
-            method: 'Put',
+    async (user, { getState }) => {
+        const token = getState().token;
+        const login = getState().user.login;
+        const response = await fetch(`${base_url}/user/${login}`, {
+            method: 'PATCH',
             body: JSON.stringify(user),
             headers: {
                 'Content-Type': 'application/json',
-                Authorization: getState().token
+                Authorization: token
             }
-        })
+        });
         if (response.status === 401) {
-            throw new Error(`login or password incorrect`)
+            throw new Error(`login or password incorrect`);
         }
         if (!response.ok) {
             throw new Error('Something went wrong');
         }
         return await response.json();
     }
-)
+);
 
-export const changePassword = createAsyncThunk<string, {newPassword: string, oldPassword: string}, {state: RootState}>(
+export const changePassword = createAsyncThunk<string, { newPassword: string, oldPassword: string }, { state: RootState }>(
     'user/password',
-    async ({newPassword, oldPassword}, {getState}) => {
-        const response = await fetch(`${base_url}/user/password`, {
-            method: 'Put',
+    async ({ newPassword, oldPassword }, { getState }) => {
+        const token = createToken(getState().user.login, oldPassword);
+        const response = await fetch(`${base_url}/password`, {
+            method: 'PATCH',
             headers: {
                 'X-Password': newPassword,
-                Authorization: createToken(getState().user.login, oldPassword)
+                Authorization: token
             }
-        })
+        });
         if (response.status === 401) {
-            throw new Error(`login or password incorrect`)
+            throw new Error(`login or password incorrect`);
         }
         if (!response.ok) {
             throw new Error('Something went wrong');
         }
-        return createToken(getState().user.login, newPassword)
+        return createToken(getState().user.login, newPassword);
     }
-)
+);
